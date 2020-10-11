6c10a439dd1ab01b4158faf004ede080d9744450
Amber Spadafora <ahelenespadafora@gmail.com>
2020-10-10 20:03:05 -0400

    Project 5 challenges

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var isShowingError = false
+    @State private var score = 0
     
     func wordError(title: String, message: String) {
         errorTitle = title
@@ -28,6 +29,8 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: fileUrl) {
                 let allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkwood"
+                usedWords = []
+                score = 0
                 return
             }
         }
@@ -39,20 +42,24 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
         guard isOriginal(word: newWord) else {
             wordError(title: "Word used already", message: "\(newWord) has been used already, fughettaboutit")
+            score -= 1
             return
         }
         
         guard isPossible(word: newWord) else {
             wordError(title: "Word isn't possible", message: "That word cannot be made from the original word, what's the matter wit' youus")
+            score -= 1
             return
         }
         
         guard isReal(word: newWord) else {
             wordError(title: "Seriously?", message: "That is not a real word ya puts")
+            score -= 1
             return
         }
         
         usedWords.insert(newWord, at: 0)
+        score += 1
         newWord = ""
     }
     
@@ -73,9 +80,12 @@ struct ContentView: View {
     }
     
     func isReal(word: String) -> Bool {
+        if word.count < 4 || word == rootWord { return false }
+        
         let textChecker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = textChecker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
         return misspelledRange.location == NSNotFound
     }
     
@@ -91,11 +101,20 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
+                Text("Score: \(score)")
                 .navigationBarTitle(Text(rootWord))
                 .onAppear(perform: startGame)
                 .alert(isPresented: $isShowingError) { () -> Alert in
                     Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
                 }
+            .navigationBarItems(leading:
+                Button(action: {
+                    self.startGame()
+                }){
+                    Text("New Game")
+                }
+                    
+                )
             }
         }
     }
